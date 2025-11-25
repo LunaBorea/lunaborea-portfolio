84582adf700bee72202765a60adc82642b9dc5e4
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2025-11-25 14:58:54 +0200

    header component

---
+++ app/components/ostroheader.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Ostroheader() {
+  return (
+    <div className="header-container">
+        <div className="logo"></div>
+        <div className="mode-toggle"></div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import Ostroheader from "./components/ostroheader"
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Ostroheader></Ostroheader>
+      <main>
+        <div className="title"></div>
+      </main>
+    </div>
   );
 }
