4b2d4ec7c6b7fc31e3172f64d3fd6c638ea1168a
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2026-01-08 11:02:35 +0200

    changed button size

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           <div className="title uppercase text-void font-extrabold text-6xl">hello, we are <span className="text-shadow-ostroborea text-lion">ostroborea</span></div>  
           <p className="text-2xl text-frenchgray max-w-1/2 text-center">a game development studio focused on fantasy, creativity and imagination</p>
         </div>
-        <Link className="button w-48 h-48 rounded-full border-lion border-8 cursor-pointer" href="./projects/"></Link>
+        <Link className="button w-42 h-42 rounded-full border-lion border-8 cursor-pointer" href="./projects/"></Link>
       </main>
     </div>
   );
